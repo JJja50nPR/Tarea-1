13d84cb5b03d8346b94edaac08155e28c4e4a1da
henryseguram <henry.segura@uhispano.ac.cr>
2022-06-04 19:07:08 -0400

    BD Agregada

--- database/database.sql
+++ database/database.sql
@@ -1,1 +1,32 @@
-CREATE DATABASE primerapi;
+CREATE DATABASE tarea1;
+
+\l
+
+\c tarea1;
+
+CREATE TABLE Estudiante (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    apellido VARCHAR(40),
+    cedula VARCHAR(11),
+    cod_provincia VARCHAR(10)
+);
+
+\dt
+
+CREATE TABLE provincia (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    cod_provincia VARCHAR(10)
+);
+
+
+INSERT INTO estudiante (name, apellido, cedula, cod_provincia)
+    VALUES ('Alejandra', 'Benitez','1-1123-1123', 'CR-1');
+
+    INSERT INTO provincia (name, cod_provincia)
+    VALUES ('Cartago', 'CR-4');
+
+select * from provincia;
+
+alter table estudiante add constraint FK_estudiante_cod_provincia foreign key ()
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,11 +1,18 @@
 const {Pool} = require('pg');
 
-new Pool({
+const pool= new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    password: 'kali',
+    database: 'tarea1',
+    port: '5432'
     
 })
 
 const getEstudiante = (req,res) => {
-    res.send('Estudiante');
+    const response = await pool.query('SELECT * FROM estudiante inner join provincia on estudiante.cod_provincia = provincia.cod_provincia ORDER BY id ASC');
+    res.status(200).json(response.rows);
+
 }
 
 
