c0ee44ae8cdfdf961c2bb0c270b83aca46e204e8
henryseguram <henry.segura@uhispano.ac.cr>
2022-06-15 21:37:51 -0400

    Relaciones Tablas

--- database/database.sql
+++ database/database.sql
@@ -4,30 +4,37 @@ CREATE DATABASE tarea1;
 
 \c tarea1;  -- Conectar Base de datos creada
 
+CREATE TABLE provincia (
+    cod_provincia VARCHAR(10) PRIMARY KEY NOT NULL,
+    name VARCHAR(40) NOT NULL
+);
+
 CREATE TABLE Estudiante (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(40),
-    apellido VARCHAR(40),
-    cedula VARCHAR(11),
-    cod_provincia VARCHAR(10)
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(40) NOT NULL,
+    apellido VARCHAR(40) NOT NULL,
+    cedula VARCHAR(11) NOT NULL,
+    cod_provincia VARCHAR(10),
+    CONSTRAINT FK_provinciaEstudiante FOREIGN KEY (cod_provincia) REFERENCES provincia (cod_provincia)
 );
 
 \dt  --Listar las tablas
 
-CREATE TABLE provincia (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(40),
-    cod_provincia VARCHAR(10)
-);
+
 
 
 INSERT INTO estudiante (name, apellido, cedula, cod_provincia)
     VALUES ('Alejandra', 'Benitez','1-1123-1123', 'CR-1'),
-           ('Sofia', 'Vergara','1-1124-3423', 'CR-2');
+            ('Henry', 'Segura','1-1125-1125', 'CR-2'),
+            ('Jeffrey', 'Porras','1-5123-5123', 'CR-4'),
+            ('Jeffrey', 'Esquivel','5-1123-1124', 'CR-1'),
+            ('Sofia', 'Mendez','3-1123-1124', 'CR-1'),
+            ('Catalina', 'Zuniga','6-1123-1124', 'CR-7'),
+            ('Manuel', 'Trejos','2-1123-1124', 'CR-6'),
+            ('Juliana', 'Mata','7-1123-9124', 'CR-1'),
+            ('Juan', 'Perez','4-1123-1124', 'CR-3'),
+           ('Jason', 'Palacios','3-1324-3423', 'CR-5');
 
     INSERT INTO provincia (name, cod_provincia)
-    VALUES ('Cartago', 'CR-4');
-
-select * from provincia;
-
-alter table estudiante add constraint FK_estudiante_cod_provincia foreign key ()
+    VALUES ('San Jose', 'CR-1'),('Alajuela', 'CR-2'),('Heredia', 'CR-3'),('Cartago', 'CR-4'),
+    ('Guanacaste', 'CR-5'),('Puntarenas', 'CR-6'),('Limon', 'CR-7');
